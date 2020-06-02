7ad099e12717db344e9e08fcff969ebe4a24279b
Fatoumata A Diaby <fatimad5293@gmail.com>
2020-06-02 22:03:30 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,12 @@
-def my_each(array)
+def my_each(array) # put argument(s) here
  counter = 0 
- while counter is < array.length 
-    yield array[counter]
-   counter += 1
- end
+ while counter < array.length # [blue, red, green, yellow]
+ yield array[counter]
+ counter += 1 
+ end 
+  # code here
  array
-end 
+end
 
 
 
