1223d76de3caca5e2350eaa473cec8cb33c727d7
Fatoumata A Diaby <fatimad5293@gmail.com>
2020-06-02 21:48:53 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,11 @@
 def my_each(array)
   counter = 0 
   while counter is < array.length 
-  yield array[counter]
-  counter += 1
+    yield array[counter]
+   counter += 1
  end
+ 
+  array
 end 
 
 
